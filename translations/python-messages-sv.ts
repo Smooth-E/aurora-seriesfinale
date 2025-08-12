5637ff34ccd9e1844b795f5fc7d035c3e8ad225f
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-08-12 15:09:21 +0200

    Update Python translations

--- translations/python-messages-sv.ts
+++ translations/python-messages-sv.ts
@@ -1,101 +1,125 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv">
+    <extra-po-header-generated_by>Babel 2.17.0</extra-po-header-generated_by>
+    <extra-po-header-language>sv</extra-po-header-language>
+    <extra-po-header-language_team>sv &lt;LL@li.org&gt;</extra-po-header-language_team>
+    <extra-po-header-last_translator>FULL NAME &lt;EMAIL@ADDRESS&gt;</extra-po-header-last_translator>
+    <extra-po-header-plural_forms>nplurals=2; plural=(n != 1);</extra-po-header-plural_forms>
+    <extra-po-header-po_revision_date>YEAR-MO-DA HO:MI+ZONE</extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date>2025-08-12 12:51+0000</extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version>PROJECT VERSION</extra-po-header-project_id_version>
+    <extra-po-header-report_msgid_bugs_to>EMAIL@ADDRESS</extra-po-header-report_msgid_bugs_to>
+    <extra-po-headers>Project-Id-Version,Report-Msgid-Bugs-To,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language,Language-Team,Plural-Forms,MIME-Version,Content-Type,Content-Transfer-Encoding,Generated-By</extra-po-headers>
 <context>
     <name></name>
     <message numerus="yes">
-        <location filename="src/SeriesFinale/series.py" line="268"/>
+        <location filename="src/SeriesFinale/series.py" line="298"/>
         <source>%s season</source>
         <translation>
             <numerusform>%s säsong</numerusform>
             <numerusform>%s säsonger</numerusform>
         </translation>
-        <extra-po-msgid_plural>%s seasons</extra-po-msgid_plural>
         <extra-po-flags>python-format</extra-po-flags>
+        <extra-po-msgid_plural>%s seasons</extra-po-msgid_plural>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="271"/>
-        <location filename="src/SeriesFinale/series.py" line="309"/>
+        <location filename="src/SeriesFinale/series.py" line="301"/>
+        <location filename="src/SeriesFinale/series.py" line="339"/>
         <source>Completely watched</source>
         <translation>Färdigsedd</translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="273"/>
+        <location filename="src/SeriesFinale/series.py" line="303"/>
         <source>Show has ended</source>
         <translation>Serien är avslutad</translation>
     </message>
     <message numerus="yes">
-        <location filename="src/SeriesFinale/series.py" line="277"/>
-        <location filename="src/SeriesFinale/series.py" line="313"/>
+        <location filename="src/SeriesFinale/series.py" line="307"/>
+        <location filename="src/SeriesFinale/series.py" line="343"/>
         <source>%s episode not watched</source>
         <translation>
             <numerusform>%s episod ej sedd</numerusform>
             <numerusform>%s episoder ej sedda</numerusform>
         </translation>
-        <extra-po-msgid_plural>%s episodes not watched</extra-po-msgid_plural>
         <extra-po-flags>python-format</extra-po-flags>
+        <extra-po-msgid_plural>%s episodes not watched</extra-po-msgid_plural>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="282"/>
-        <location filename="src/SeriesFinale/series.py" line="318"/>
+        <location filename="src/SeriesFinale/series.py" line="312"/>
+        <location filename="src/SeriesFinale/series.py" line="348"/>
         <source>No episodes to watch</source>
         <translation>Inga episoder att se</translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="286"/>
+        <location filename="src/SeriesFinale/series.py" line="316"/>
         <source>&lt;i&gt;Next:&lt;/i&gt; %s, %s</source>
         <translation>&lt;i&gt;Nästa:&lt;/i&gt; %s, %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="290"/>
+        <location filename="src/SeriesFinale/series.py" line="320"/>
         <source>&lt;i&gt;Next:&lt;/i&gt; %s</source>
         <translation>&lt;i&gt;Nästa:&lt;/i&gt; %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="298"/>
+        <location filename="src/SeriesFinale/series.py" line="328"/>
         <source>Special</source>
         <translation>Special</translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="300"/>
+        <location filename="src/SeriesFinale/series.py" line="330"/>
         <source>Season %s</source>
         <translation>Säsong %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="322"/>
+        <location filename="src/SeriesFinale/series.py" line="352"/>
         <source>&lt;i&gt;Next episode:&lt;/i&gt; %s, %s</source>
         <translation>&lt;i&gt;Nästa episod:&lt;/i&gt; %s, %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="326"/>
+        <location filename="src/SeriesFinale/series.py" line="356"/>
         <source>&lt;i&gt;Next episode:&lt;/i&gt; %s</source>
         <translation>&lt;i&gt;Nästa episod:&lt;/i&gt; %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="420"/>
+        <location filename="src/SeriesFinale/series.py" line="450"/>
         <source>Ep. %s: %s</source>
         <translation>Ep. %s: %s</translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="467"/>
+        <location filename="src/SeriesFinale/series.py" line="497"/>
         <source>Today</source>
         <translation>I dag</translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="469"/>
+        <location filename="src/SeriesFinale/series.py" line="499"/>
         <source>Tomorrow</source>
         <translation>I morgon</translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="471"/>
+        <location filename="src/SeriesFinale/series.py" line="501"/>
         <source>Yesterday</source>
         <translation>I går</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="508"/>
+        <source>%d %b %Y</source>
+        <extracomment>TRANSLATORS:This is a Python-formatted date (day, month, year).Translate it to the form preferred in your language.See: https://strftime.org</extracomment>
+        <translation type="unfinished"></translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="513"/>
+        <source>%d %b</source>
+        <extracomment>TRANSLATORS: This is a Python-formatted date (day and month only).Translate it to the form preferred in your language.See: https://strftime.org</extracomment>
+        <translation type="unfinished"></translation>
+        <extra-po-flags>python-format</extra-po-flags>
     </message>
 </context>
 </TS>
--- translations/python-messages.ts
+++ translations/python-messages.ts
@@ -1,10 +1,24 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+    <extra-po-header-generated_by>Babel 2.17.0</extra-po-header-generated_by>
+    <extra-po-header-language_team>LANGUAGE &lt;LL@li.org&gt;</extra-po-header-language_team>
+    <extra-po-header-last_translator>FULL NAME &lt;EMAIL@ADDRESS&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date>YEAR-MO-DA HO:MI+ZONE</extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date>2025-08-12 12:51+0000</extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version>harbour-seriesfinale VERSION</extra-po-header-project_id_version>
+    <extra-po-header-report_msgid_bugs_to>EMAIL@ADDRESS</extra-po-header-report_msgid_bugs_to>
+    <extra-po-header_comment># Translations template for harbour-seriesfinale.
+# Copyright (C) 2025 harbour-seriesfinale contributors
+# This file is distributed under the same license as the harbour-seriesfinale project.
+# FIRST AUTHOR &lt;EMAIL@ADDRESS&gt;, 2025.
+#
+#, fuzzy</extra-po-header_comment>
+    <extra-po-headers>Project-Id-Version,Report-Msgid-Bugs-To,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,MIME-Version,Content-Type,Content-Transfer-Encoding,Generated-By</extra-po-headers>
 <context>
     <name></name>
     <message numerus="yes">
-        <location filename="src/SeriesFinale/series.py" line="268"/>
+        <location filename="src/SeriesFinale/series.py" line="298"/>
         <source>%s season</source>
         <translation type="unfinished">
             <numerusform></numerusform>
@@ -13,19 +27,19 @@
         <extra-po-msgid_plural>%s seasons</extra-po-msgid_plural>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="271"/>
-        <location filename="src/SeriesFinale/series.py" line="309"/>
+        <location filename="src/SeriesFinale/series.py" line="301"/>
+        <location filename="src/SeriesFinale/series.py" line="339"/>
         <source>Completely watched</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="273"/>
+        <location filename="src/SeriesFinale/series.py" line="303"/>
         <source>Show has ended</source>
         <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
-        <location filename="src/SeriesFinale/series.py" line="277"/>
-        <location filename="src/SeriesFinale/series.py" line="313"/>
+        <location filename="src/SeriesFinale/series.py" line="307"/>
+        <location filename="src/SeriesFinale/series.py" line="343"/>
         <source>%s episode not watched</source>
         <translation type="unfinished">
             <numerusform></numerusform>
@@ -34,66 +48,80 @@
         <extra-po-msgid_plural>%s episodes not watched</extra-po-msgid_plural>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="282"/>
-        <location filename="src/SeriesFinale/series.py" line="318"/>
+        <location filename="src/SeriesFinale/series.py" line="312"/>
+        <location filename="src/SeriesFinale/series.py" line="348"/>
         <source>No episodes to watch</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="286"/>
+        <location filename="src/SeriesFinale/series.py" line="316"/>
         <source>&lt;i&gt;Next:&lt;/i&gt; %s, %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="290"/>
+        <location filename="src/SeriesFinale/series.py" line="320"/>
         <source>&lt;i&gt;Next:&lt;/i&gt; %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="298"/>
+        <location filename="src/SeriesFinale/series.py" line="328"/>
         <source>Special</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="300"/>
+        <location filename="src/SeriesFinale/series.py" line="330"/>
         <source>Season %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="322"/>
+        <location filename="src/SeriesFinale/series.py" line="352"/>
         <source>&lt;i&gt;Next episode:&lt;/i&gt; %s, %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="326"/>
+        <location filename="src/SeriesFinale/series.py" line="356"/>
         <source>&lt;i&gt;Next episode:&lt;/i&gt; %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="420"/>
+        <location filename="src/SeriesFinale/series.py" line="450"/>
         <source>Ep. %s: %s</source>
         <translation type="unfinished"></translation>
         <extra-po-flags>python-format</extra-po-flags>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="467"/>
+        <location filename="src/SeriesFinale/series.py" line="497"/>
         <source>Today</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="469"/>
+        <location filename="src/SeriesFinale/series.py" line="499"/>
         <source>Tomorrow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/SeriesFinale/series.py" line="471"/>
+        <location filename="src/SeriesFinale/series.py" line="501"/>
         <source>Yesterday</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="508"/>
+        <source>%d %b %Y</source>
+        <extracomment>TRANSLATORS:This is a Python-formatted date (day, month, year).Translate it to the form preferred in your language.See: https://strftime.org</extracomment>
+        <translation type="unfinished"></translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="513"/>
+        <source>%d %b</source>
+        <extracomment>TRANSLATORS: This is a Python-formatted date (day and month only).Translate it to the form preferred in your language.See: https://strftime.org</extracomment>
+        <translation type="unfinished"></translation>
+        <extra-po-flags>python-format</extra-po-flags>
     </message>
 </context>
 </TS>
